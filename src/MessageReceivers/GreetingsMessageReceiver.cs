a012a64c961a6438ce2ef0e85ea126bfb2664d61
Nicolas Bontempo <nicolas.bomtempo@gmail.com>
2018-07-01 23:23:49 -0300

    first commit

---
+++ src/MessageReceivers/GreetingsMessageReceiver.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Lime.Protocol;
+using Take.Blip.Client;
+
+namespace TaskBot.MessageReceivers
+{
+    /// <summary>
+    /// Defines a class for handling messages. 
+    /// This type must be registered in the application.json file in the 'messageReceivers' section.
+    /// </summary>
+    public class GreetingsMessageReceiver : IMessageReceiver
+    {
+        private readonly ISender _sender;
+        private readonly Settings _settings;
+
+        public GreetingsMessageReceiver(ISender sender, Settings settings)
+        {
+            _sender = sender;
+            _settings = settings;
+        }
+
+        public async Task ReceiveAsync(Message message, CancellationToken cancellationToken)
+        {
+            Trace.TraceInformation($"From: {message.From} \tContent: {message.Content}");
+            await _sender.SendMessageAsync("Pong!", message.From, cancellationToken);
+        }
+    }
+}
